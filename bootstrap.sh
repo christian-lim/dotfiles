dc91622bd9bf38cfc476747b7b568873d3681bfa
climc118 <cxl5524@psu.edu>
2020-06-07 03:20:43 -0400

    init commit

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+function bootstrap() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude ".bash_profile_preserve" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE-MIT.txt" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	bootstrap;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		bootstrap;
+	fi;
+fi;
+unset bootstrap;
